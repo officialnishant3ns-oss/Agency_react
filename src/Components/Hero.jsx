ffa56ed9ce84bfb523e667822134c54f55600541
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-28 00:08:17 +0530

    completed hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -19,10 +19,17 @@ function Hero() {
             <div className='pt-15 max-sm:pt-10'>
                 <img className="w-full max-w-7xl" src={assets.hero_img} alt="" />
             </div>
-            <h2 className='text-3xl font-extrabold dark:text-white mt-13 max-sm:text-xl'>Trusted By Leading Companies</h2>
-            <div>
-                <img src={company_logos.hero_img} alt="" />
+            <h2 className='text-3xl font-extrabold dark:text-white mt-15 max-sm:text-xl'>Trusted By Leading Companies</h2>
+            
+            <div className='flex justify-center items-center mt-10 gap-x-15 max-sm:flex-wrap max-sm:gap-y-7'>
+                {company_logos.map((logo, index) => (
+                    <img
+                        key={index} src={logo}
+                        alt="" className="w-25 h-8 max-sm:h-6  max-sm:w-18 gap-y-20 "
+                    />
+                ))}
             </div>
+
         </div>
     )
 }
